1b8cc3e6e157b18883d5d0db61a7d3979d746f02
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-04-20 17:11:18 -0300

    TypeOrm

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { SettingsController } from "./controllers/SettingsController";
+
+const routes = Router();
+
+const settingController = new SettingsController();
+
+routes.post("/settings", settingController.create) 
+
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
 import express from 'express';
+import "./database";
+
+import { routes } from './routes';
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({
-        message: "Ola nlw 05"
-    })
-})
+app.use(express.json());
 
-app.post('/users', (req, res) => {
-    return res.json({
-        message: "usuario salvo com sucesso"
-    })
-})
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port  3333'));
